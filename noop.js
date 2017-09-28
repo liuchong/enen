afc0578e34b83c57b42b2376b57b60fe77338f4a
Liu Chong <mail@clojure.cn>
2017-09-29 02:45:33 +0800

    update noop.js

--- noop.js
+++ noop.js
@@ -16,10 +16,30 @@
  *   noop.f(2) // => undefined
  *   noop.f.g(3) // => undefined
  *
- * doctest not available for now
+ * Doctest not available for now
  */
-const noop = new Proxy(() => {}, {
-  get: () => noop
+const noop = new Proxy(function () {}, {
+  get
 })
+
+// The getter for proxy noop
+function get (target, name) {
+  // recursively return noop by default
+  if (!(name in target)) {
+    return noop
+  }
+  // if normal-attribute (not funciton) was set, directly return it
+  if (typeof target[name] !== 'function') {
+    return target[name]
+  }
+  // if a function-attribute "f" was set, make a new proxy "p".
+  // When "p" is called, it will call "f" but never use it's result.
+  return new Proxy(
+    function () {
+      target[name]()
+    },
+    { get }
+  )
+}
 
 module.exports = noop
--- noop.test.js
+++ noop.test.js
@@ -2,16 +2,34 @@
 
 const noop = require('./noop')
 
-describe('noop', () => {
+describe('The basic functions of noop', () => {
   test('should return undefined when direct call noop', () => {
-    expect(noop()).toBe(undefined)
+    expect(noop()).toBeUndefined()
   })
   test('should return undefined when call noop with argument(s)', () => {
-    expect(noop(1)).toBe(undefined)
-    expect(noop(9, null, {}, 'what ever')).toBe(undefined)
+    expect(noop(1)).toBeUndefined()
+    expect(noop(9, null, {}, 'what ever')).toBeUndefined()
   })
   test('should return undefined when call noop.f/noop.f.g', () => {
-    expect(noop.f(2)).toBe(undefined)
-    expect(noop.f.g()).toBe(undefined)
+    expect(noop.f(2)).toBeUndefined()
+    expect(noop.f.g()).toBeUndefined()
+  })
+})
+
+describe('When attributes are added', () => {
+  test('should keep returning undefined when called', () => {
+    noop.f = () => {}
+    expect(noop()).toBeUndefined()
+    expect(noop.f()).toBeUndefined()
+  })
+  test('should keep get noop by point operation', () => {
+    noop.f = () => {}
+    expect(noop.f).not.toBe(noop)
+    expect(noop.f.g).toBe(noop)
+  })
+  test('should keep what it is when a not-function-attribute is set', () => {
+    const obj = {}
+    noop.o = obj
+    expect(noop.o).toBe(obj)
   })
 })
